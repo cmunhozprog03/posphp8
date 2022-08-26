728d855a1b18a06ae5f2f9b710e6853da17a98ed
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-08-26 18:29:40 -0300

    Aula 30

--- Views/modelo/footer.php
+++ Views/modelo/footer.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
+      <b>Versão</b> 1.1.3
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">EPBTI</a>.</strong> Todo os direitos reservados.
   </footer>
 
   <!-- Control Sidebar -->
--- Views/modelo/nav.php
+++ Views/modelo/nav.php
@@ -1,50 +1,101 @@
-<nav class="main-header navbar navbar-expand navbar-dark navbar-dark">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="Views/index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-    </ul>
+<nav class="main-header navbar
+    navbar-expand
+    navbar-dark navbar-dark">
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
-      <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="navbar-search-block">
-          <form class="form-inline">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
+
+  <ul class="navbar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#">
+        <i class="fas fa-bars"></i>
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+    </li>
+
+
+
+  </ul>
+
+
+  <ul class="navbar-nav ml-auto">
+
+
+
+    <li class="nav-item">
+
+
+      <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+        <i class="fas fa-search"></i>
+      </a>
+
+
+      <div class="navbar-search-block">
+        <form class="form-inline" action="#" method="get">
+          <input type="hidden" name="_token" value="OTzcA2ZukG8EoBNK3s5snAKeM8zWROdwfP28Z6zz">
+
+          <div class="input-group">
+
+
+            <input class="form-control form-control-navbar" type="search" name="adminlteSearch" placeholder="search" aria-label="search">
+
+
+            <div class="input-group-append">
+              <button class="btn btn-navbar" type="submit">
+                <i class="fas fa-search"></i>
+              </button>
+              <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                <i class="fas fa-times"></i>
+              </button>
             </div>
+
+          </div>
+        </form>
+      </div>
+
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+        <i class="fas fa-expand-arrows-alt"></i>
+      </a>
+    </li>
+
+
+
+    <li class="nav-item dropdown user-menu">
+
+
+      <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+        <span>
+          Christovam Munhoz
+        </span>
+      </a>
+
+
+      <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+
+
+
+
+
+
+
+
+        <li class="user-footer">
+          <a class="btn btn-default btn-flat float-right  btn-block " href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <i class="fa fa-fw fa-power-off text-red"></i>
+            Log Out
+          </a>
+          <form id="logout-form" action="http://127.0.0.1:8000/logout" method="POST" style="display: none;">
+            <input type="hidden" name="_token" value="OTzcA2ZukG8EoBNK3s5snAKeM8zWROdwfP28Z6zz">
           </form>
-        </div>
-      </li>
+        </li>
 
-    </ul>
+      </ul>
 
-    <!-- Perfil do usuário -->
-    <div class="nav-custom-menu">
-      <li class="dropdown user user-menu">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-          <img src="Views/img/usuarios/default/anonymous.png" alt="" class="user-image" width="15%">
-          <span class="hidden-xs">Administrador</span>
-        </a>
-      </li>
-    </div>
-  </nav>
+    </li>
+
+
+  </ul>
+
+</nav>
