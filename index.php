36e95a3bc525674ce1137ee97dd1cc27d123f4c9
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-09-01 12:13:23 -0300

    update01-09-2022

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
 require_once "Controllers/ControladorModelo.php";
+require_once "Controllers/usuarios.controlador.php";
+require_once "Controllers/clientes.controlador.php";
+require_once "Controllers/produtos.controlador.php";
+require_once "Controllers/vendas.controlador.php";
+require_once "Controllers/categorias.controlador.php";
 
 $modelo = new ControladorModelo();
 
